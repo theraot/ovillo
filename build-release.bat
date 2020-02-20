208512396332c69eb385a2768699ca4add5e494a
Theraot <Theraot@gmail.com>
2020-02-20 13:58:43 -0500

    Handling multiple arguments

--- build-release.bat
+++ build-release.bat
@@ -5,38 +5,37 @@ SET batpath=%basepath%\bat
 IF not exist "%batpath%" GOTO NoBatFolder
 
 pushd %basepath%
-        CALL "%batpath%\debug-ensure-buildable.bat"
-        CALL "%batpath%\debug-build-clean.bat"
+    CALL "%batpath%\debug-ensure-buildable.bat"
+    CALL "%batpath%\debug-build-clean.bat"
+popd
 
-        IF %1.==. GOTO No1
-        pushd %1
-            SET rootpath=%cd%
-            IF "%rootpath:~-1%"=="\" SET "rootpath=%rootpath:~0,-1%"
-        popd
-        CALL :AddSource %rootpath%
-        GOTO Work
-
-    :No1
-        SET rootpath=%~dp0
-        IF "%rootpath:~-1%"=="\" SET "rootpath=%rootpath:~0,-1%"
-        SET rootpath=%rootpath%\src
-        CALL :AddSource %rootpath%
-        GOTO Work
-
-    :Work
+    IF %1.==. GOTO No1Add
+    FOR %%A IN (%*) DO CALL :AddSource %%A
+    GOTO Work
+:No1Add
+    call :AddSource %~dp0\src
+    GOTO Work
+:Work
+    pushd %basepath%
         pushd "%cd%\.obj\"
             echo Compiling TypeScript
             CALL tsc -p tsconfig.json
         popd
-        CALL "%batpath%\debug-build-complete.bat" %rootpath%
-        GOTO PostWork
+    popd
+    IF %1.==. GOTO No1Complete
+    FOR %%A IN (%*) DO CALL :CompleteSource %%A
+    GOTO Postwork
+:No1Complete
+    call :CompleteSource %~dp0\src
+    GOTO Postwork
 
-    :PostWork
+:PostWork
+    pushd %basepath%
         CALL "%batpath%\release-ensure-buildable.bat"
         CALL "%batpath%\release-build-clean.bat"
         CALL "%batpath%\release-from-debug.bat"
-popd
-Goto End
+    popd
+    Goto End
 
 :NoBatFolder
     echo Not found bat folder, expected path "%batpath%"
@@ -44,9 +43,33 @@ Goto End
     GOTO End
 
 :AddSource
-    echo Working on: "%~1"
-    If not exist "%~1" GOTO NoSource
-    CALL "%batpath%\debug-build-add.bat" %~1
+    pushd %~1
+        SET rootpath=%cd%
+    popd
+    IF "%rootpath:~-1%"=="\" SET "rootpath=%rootpath:~0,-1%"
+    If not exist "%rootpath%" GOTO NoSource
+    echo Working on: "%rootpath%"
+    pushd %basepath%
+        CALL "%batpath%\debug-build-add.bat" %rootpath%
+    popd
+    GOTO AddSourceEnd
+    :NoSource
+        echo Not found source folder, expected path: "%rootpath%"
+        echo Currnet script: %~f0
+        GOTO AddSourceEnd
+    :AddSourceEnd
+GOTO:eof
+
+:CompleteSource
+    pushd %~1
+        SET rootpath=%cd%
+    popd
+    IF "%rootpath:~-1%"=="\" SET "rootpath=%rootpath:~0,-1%"
+    If not exist "%rootpath%" GOTO NoSource
+    echo Working on: "%rootpath%"
+    pushd %basepath%
+        CALL "%batpath%\debug-build-complete.bat" %rootpath%
+    popd
     GOTO AddSourceEnd
     :NoSource
         echo Not found source folder, expected path: "%rootpath%"
