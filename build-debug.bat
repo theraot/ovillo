3f568b25d6ecf97420b673cbb0b671c804037d7e
Theraot <Theraot@gmail.com>
2020-02-20 13:15:29 -0500

    Whitespace

--- build-debug.bat
+++ build-debug.bat
@@ -5,36 +5,36 @@ SET batpath=%basepath%\bat
 IF not exist "%batpath%" GOTO NoBatFolder
 
 pushd %basepath%
-    IF %1.==. GOTO No1
-    pushd %1
-        SET rootpath=%cd%
+        IF %1.==. GOTO No1
+        pushd %1
+            SET rootpath=%cd%
+            IF "%rootpath:~-1%"=="\" SET "rootpath=%rootpath:~0,-1%"
+        popd
+        GOTO Work
+
+    :No1
+        SET rootpath=%~dp0
         IF "%rootpath:~-1%"=="\" SET "rootpath=%rootpath:~0,-1%"
-    popd
-    GOTO Work
+        SET rootpath=%rootpath%\src
+        GOTO Work
 
-:No1
-    SET rootpath=%~dp0
-    IF "%rootpath:~-1%"=="\" SET "rootpath=%rootpath:~0,-1%"
-    SET rootpath=%rootpath%\src
-    GOTO Work
+    :Work
+        echo Working on: "%rootpath%"
+        If not exist "%rootpath%" GOTO NoSource
+        CALL "%batpath%\debug-ensure-buildable.bat"
+        CALL "%batpath%\debug-build-clean.bat"
+        CALL "%batpath%\debug-build-add.bat" %rootpath%
+        pushd "%cd%\.obj\"
+            echo Compiling TypeScript
+            CALL tsc -p tsconfig.json
+        popd
+        CALL "%batpath%\debug-build-complete.bat" %rootpath%
+        GOTO End
 
-:Work
-    echo Working on: "%rootpath%"
-    If not exist "%rootpath%" GOTO NoSource
-    CALL "%batpath%\debug-ensure-buildable.bat"
-    CALL "%batpath%\debug-build-clean.bat"
-    CALL "%batpath%\debug-build-add.bat" %rootpath%
-    pushd "%cd%\.obj\"
-        echo Compiling TypeScript
-        CALL tsc -p tsconfig.json
-    popd
-    CALL "%batpath%\debug-build-complete.bat" %rootpath%
-    GOTO End
-
-:NoSource
-    echo Not found source folder, expected path: "%rootpath%"
-    echo Currnet script: %~f0
-    GOTO End
+    :NoSource
+        echo Not found source folder, expected path: "%rootpath%"
+        echo Currnet script: %~f0
+        GOTO End
 popd
 
 :NoBatFolder
